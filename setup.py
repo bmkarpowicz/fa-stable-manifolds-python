e18c5e855035591cdf11c76cb05d7373ed4cfd9b
bkarpowicz3 <brianna.karpowicz@gmail.com>
2025-04-21 09:50:11 -0400

    update code

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='fa_stable_manifolds_python',
+    version='0.0.0',
+    packages=['fa_stable_manifolds_python'],
+    author="Systems Neural Engineering Lab",
+)
